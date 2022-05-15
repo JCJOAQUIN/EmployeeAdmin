2d0ddbc25647ade34cb94836aeeb1271f2f5a85f
I.S.C. JOSE CARLOS JOAQUIN VAZQUEZ <iscjovc0125@gmail.com>
2022-05-15 18:23:25 -0500

    Creating project

---
+++ storage/framework/views/960771c52a704eadd5a0e91a1ccb98ee09d26923.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="text-red-200">hello word</div>
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+</html>
+<?php /**PATH /opt/lampp/htdocs/EmployeeAdmin/resources/views/welcome.blade.php ENDPATH**/ ?>
