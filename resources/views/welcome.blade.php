1675d992d81e033e2885e8dca7344c9acc8f4499
I.S.C. JOSE CARLOS JOAQUIN VAZQUEZ LINUX <josecarlos19882546@gmail.com>
2022-06-08 00:14:52 -0500

    Update git ignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>EMSYS Login</title>
 </head>
 <body>
-    <div class="text-red-200">hello word</div>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <header>
+        <div class="navBar bg-blue-500 flex items-center">
+            <div class="mx-4 perfil-icon w-12 h-12 bg-white ml-2 border-2 border-stone-400 rounded-full"></div>
+            <div class="block text-center text-white">
+                <div class="text-lg font-bold">Jose Carlos joaquin vazquez</div>
+                <div>Administrador</div>
+            </div>
+            <div class="inline-flex justify-end w-full text-white">
+                <div class="px-4">Home</div>
+                <div class="px-4">Employees</div>
+                <div class="px-4">Times</div>
+                <div class="px-4">About</div>
+            </div>
+        </div>
+    </header>
 </body>
 </html>
