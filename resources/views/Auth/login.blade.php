bbc3be414432a5fe75e1cecc17bd4d8fb2ea783a
José Carlos Joaquín Vazquez <josecarlos19882546@gmail.com>
2022-06-14 16:03:41 -0500

    Define colors pallete

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -7,12 +7,15 @@
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 	<title>Login</title>
 </head>
-<body class="bg-darkDefault">
+<body class="bg-light">
 	<header>
+
+
+        <div></div>
 		<div class="bg-primary grid grid-cols-3 w-full py-4">
-			<div class="text-white md:text-xl text-md text-left ml-12">Login</div>
-			<div class="text-white md:text-2xl sm:text-xl xs:text-sm text-center">Employee Admin Sistem</div>
-			<div class="text-white md:text-xl text-md text-right mr-12 grid md:grid-cols-4 grid-cols-1">
+			<div class="text-light md:text-xl text-md text-left ml-12 font-semibold">Login</div>
+			<div class="text-light md:text-2xl sm:text-xl xs:text-sm text-center font-semibold">Employee Admin Sistem</div>
+			<div class="text-light md:text-xl text-md text-right mr-12 grid md:grid-cols-4 grid-cols-1">
 				<div class="col-span-1 md:col-start-3 col-start-2 text-center">
 					<a href="#"><button type="button">About</button></a>
 				</div>
@@ -23,22 +26,22 @@
 		</div>
 	</header>
     <div></div>
-	<div class="grid grid-cols-11 mt-8">
+	<div class="grid grid-cols-11 mt-12">
 		<div class="col-span-4 col-start-2 w-full id="left">
 			<div id="enterprise-image" class="w-full h-60 text-center mb-8">
-				imagen del empleado
+
 			</div>
 			<div class="user-form block mt-4">
 				<div>
-					<label class="font-bold text-lg text-gray-700">User</label>
+					<label class="font-bold text-lg text-darkSoft">User</label>
 				</div>
 				<div>
-					<input type="text" name="user" class="w-full h-12 rounded-md px-4" placeholder="Introduzca su nombre de usuario">
+					<input type="text" name="user" class="w-full bg-lightSoft h-12 rounded-md px-4" placeholder="Introduzca su nombre de usuario">
 				</div>
 			</div>
 			<div class="user-form block mt-4">
 				<div>
-					<label class="font-bold text-lg text-gray-700">Password</label>
+					<label class="font-bold text-lg text-darkSoft">Password</label>
 				</div>
 				<div>
 					<input type="password" name="user" class="w-full h-12 rounded-md px-4" placeholder="Introduzca su contraseña">
@@ -49,33 +52,34 @@
                     <div class="col-span-1">
                         <div class="inline-block">
                             <div>
-                                <button class="bg-green-600 w-40 h-10 rounded-md font-bold text-white">Firmar</button>
+                                <button class="btnSign bg-secondary w-40 h-10 rounded-md font-bold text-lg text-light">Sign</button>
                             </div>
                             <div class="mt-2">
-                                <a href="#" class="text-blue-600">¿Olvidaste tu contraseña?</a>
+                                <a href="#" class=" text-darkSoft font-semibold">Forget your password?</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-span-1">
                         <div class="inline-block">
                             <div>
-                                <button class="font-bold w-40 h-10 text-gray-500">Registrarse</button>
+                                <button class="btnRegister font-bold w-40 h-10 text-darkSoft text-lg">Register</button>
                             </div>
-                            <div class="mt-2">
-                                <a href="#">Click aquí para recuperarla</a>
+                            <div class="mt-2 text-primary font-semibold">
+                                <a href="#">Click here to restore!</a>
                             </div>
                         </div>
                     </div>
                 </div>
 			</div>
 		</div>
-		<div class="col-span-4 col-start-7 rounded-md bg-white">
+		<div class="col-span-4 col-start-7 rounded-md bg-lightSoft">
 			<div class="w-full  text-center content-center rounded-md flex justify-center p-8">
-				<div class="w-full rounded-md h-24 bg-gray-500 flex item-center">
-                    <input type="text" class="digitalClock text-white bg-transparent text-center w-full text-5xl" value="9:00 AM">
+				<div class="w-full rounded-md h-24 bg-light flex item-center">
+                    <input type="text" class="digitalClock text-third bg-transparent text-center w-full text-5xl font-semibold" value="9:00 AM">
 				</div>
 			</div>
 		</div>
 	</div>
+    {{-- <script src="{{ asset('js/jquery-ui.js') }}"></script> --}}
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,31 @@
 module.exports = {
   purge: [
-      './resources/**/*.blade.php',
-     './resources/**/*.js',
-     './resources/**/*.vue',
+	  './resources/**/*.blade.php',
+	 './resources/**/*.js',
+	 './resources/**/*.vue',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+	colors: {
+		transparent:    'transparent',
+		primary:        '#196AFF',
+		primarySoft:    '#6ba4ff',
+		secondary:      '#00A82D',
+		secondarySoft:  '#49A875',
+		third:          '#434F45',
+		thirdSoft:      '#6F8272',
+		dark:           '#1B1C1C',
+		darkSoft:       '#426969',
+		light:          '#EBEEF0',
+		lightSoft:      '#ffffff',
+		danger:         '#d61c0f',
+		dangerSoft:     '#ff5d52',
+        darkDefault:    'E5E5E5',
+	},
+	extend: {},
   },
   variants: {
-    extend: {},
+	extend: {},
   },
   plugins: [],
 }
