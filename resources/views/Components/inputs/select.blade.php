b509634b52dcdccee73cdf41fe739cd60e6c2f6e
José Carlos Joaquín Vazquez <josecarlos19882546@gmail.com>
2022-10-20 00:37:12 -0500

    Add validation to Component select

--- resources/views/Components/inputs/select.blade.php
+++ resources/views/Components/inputs/select.blade.php
@@ -3,7 +3,7 @@
     <select class="relativetext-third h-10 rounded-md font-semibold focus:outline-none focus:ring-2 ring-primarySoft ring-1 transition ease-in duration-150 @if(isset($classSelect)) {{$classSelect}} @endif" @if (isset($attributeSelect)) {!! $attributeSelect !!} @endif">
         @if (isset($options))
             @foreach ($options as $option)
-                <option value={{$option["value"]}}>{{$option["content"]}}</option>
+                <option value={{$option["value"]}} @isset($option['selected']) selected @endisset>{{$option["content"]}}</option>
             @endforeach
         @endif
     </select>
