34bd26c30d713fff6447bd66ac3165689f0a8722
José Carlos Joaquín Vazquez <josecarlos19882546@gmail.com>
2022-12-03 02:24:47 -0600

    Create migration to create table employees

---
+++ database/migrations/2022_12_02_221349_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('clabe')->nullable();
+            $table->string('alias')->nullable();
+            $table->unsignedInteger('userId')->nullable();
+            $table->string('area')->nullable();
+            $table->string('department')->nullable();
+            $table->string('password')->nullable();
+            $table->foreign('userId')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
